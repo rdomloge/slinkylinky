62af0b4721a0378652257134c5b8dfefe944442e
Ramsay.Domloge <rdomloge@gmail.com>
2023-09-14 09:00:26 +0100

    first commit

---
+++ react/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>Catholicon 2.0</div>
+  );
+}
+
+export default App;
---
+++ react/src/Club.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Contact from './contact/Contact';
+import ClubSession from './ClubSession';
+
+class Club extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            club: {}
+        };
+    }
+
+    componentDidMount() {
+        const url = 'http://smy-server.10.0.0.12.xip.io/clubs/search/findClubByClubIdAndSeasonId?clubId=13&season=0';
+
+        fetch(url)
+            .then(res => res.json())
+            .then((result) => this.setState({
+                    isLoaded: true, 
+                    club: result})
+            ).catch(error => this.state.error = error);
+    }
+
+    render() {
+        const { error, isLoaded, club } = this.state;
+        if (error != null) {
+          return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Loading...</div>;
+        } else {
+          return (
+            <div>
+                {this.state.club.clubName}
+                Club sessions
+                <div>
+                    {club.clubSessions.map(sesh => <ClubSession {...sesh}{...{title: 'Club sessions'}} key="{sesh.days}"/>)}
+                </div>
+                <div>
+                    {club.matchSessions.map(sesh => <ClubSession {...sesh}{...{title: 'Match sessions'}} key="{sesh.days}"/>)}
+                </div>
+
+                
+                <Contact {...this.state.club.chairMan}{...{role: 'Chairman'}} key="1"/>
+                <Contact {...this.state.club.treasurer}{...{role: 'Treasurer'}} key="2"/>
+                <Contact {...this.state.club.secretary}{...{role: 'Secretary'}} key="3"/>
+                <Contact {...this.state.club.matchSec}{...{role: 'Match Secretary'}} key="4"/>
+            </div>
+          );
+        }
+      }
+ }
+
+ export default Club;
