2971f28941ce4207f2aff638e7fa4a8eb4934437
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-20 11:47:17 +0000

    Switched to supplierlist being provided data externally

--- react/src/pages/supplier/search/[linkdemandid].js
+++ react/src/pages/supplier/search/[linkdemandid].js
@@ -8,40 +8,17 @@ import SupplierCard from '@/components/supplier'
 export default function App() {
     const router = useRouter()
     const [demand, setDemand] = useState(null);
+    const [suppliers, setSuppliers] = useState(null);
 
     useEffect(
         () => {
             if(router.isReady) {
                 const demandUrl = 'http://localhost:8080/linkdemands/'+ router.query.linkdemandid;
-                console.log(demandUrl);
                 fetch(demandUrl)
                     .then((response) => response.json())
                     .then((json) => setDemand(json));
-        ***REMOVED*** 
-            else {
-                console.log('Router not ready yet')
-        ***REMOVED***
-    ***REMOVED***, [router.isReady]
-    );
 
-    return (
-        <div>
-            <LinkDemandCard linkdemand={demand} />
-            <SupplierList/>
-        </div>
-    );
-}
-
-function SupplierList() {
-    const router = useRouter()
-    const [suppliers, setSuppliers] = useState(null);
-    
-
-    useEffect(
-        () => {
-            if(router.isReady) {
                 const suppliersUrl = 'http://localhost:8080/bloggers/search/findBloggersForLinkDemandId?linkDemandId='+ router.query.linkdemandid;
-                console.log(suppliersUrl)
                 fetch(suppliersUrl)
                     .then((response) => response.json())
                     .then((json) => setSuppliers(json));
@@ -52,6 +29,16 @@ function SupplierList() {
     ***REMOVED***, [router.isReady]
     );
 
+    return (
+        <div>
+            <LinkDemandCard linkdemand={demand} />
+            <SupplierList suppliers={suppliers}/>
+        </div>
+    );
+}
+
+function SupplierList({suppliers}) {
+    
     if(suppliers === null) return <p>Loading...</p>;
     else return (
         <div>
