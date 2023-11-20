32e82ec110a4000f90b81280f725a87c40b81f1d
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-20 11:34:54 +0000

    Debugging

--- react/src/app/page.js
+++ react/src/app/page.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-import LinkDemand from './linkdemand/linkdemand'
+import LinkDemand from './linkdemand/LinkDemand'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-       Demand
+       Demanderoos
         <LinkDemand/>
       </div>
     </main>
