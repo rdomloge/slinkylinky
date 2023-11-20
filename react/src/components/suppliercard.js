9074045e55b3fb62ef5119ed9816ac3eb5cea18d
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-20 12:50:37 +0000

    Renamed

--- react/src/components/suppliercard.js
+++ react/src/components/suppliercard.js
@@ -4,8 +4,8 @@ import React, {useState, useEffect} from 'react'
 
 export default function SupplierCard({supplier}) {
     return (
-        <div>
-            <div>Supplier: {supplier.name}</div>
+        <div className="card">
+            <div>{supplier.name}</div>
             <div>DA: {supplier.da}</div>
             <div>Website: {supplier.website}</div>
             <div>Email: {supplier.email}</div>
