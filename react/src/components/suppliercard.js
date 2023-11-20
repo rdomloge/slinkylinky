d0982ff21752d31edaacfae5354586e5a7be2658
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-20 20:09:49 +0000

    Added some values that Chris wanted

--- react/src/components/suppliercard.js
+++ react/src/components/suppliercard.js
@@ -9,6 +9,10 @@ export default function SupplierCard({supplier}) {
             <div>DA: {supplier.da}</div>
             <div>Website: {supplier.website}</div>
             <div>Email: {supplier.email}</div>
+            <div>Fee: &pound;{supplier.weWriteFee}</div>
+            <div>SEM rush authority: {supplier.semRushAuthorityScore}</div>
+            <div>SEM rush UK monthly: {supplier.semRushUkMonthlyTraffic}</div>
+            <div>SEM rush UK Jan'23: {supplier.semRushUkJan23Traffic}</div>
             <CategoriesCard categoriesUrl={supplier._links.categories.href}/>
         </div>
     )
