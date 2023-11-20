f0adf18cf0837776805744851c5f2910eecdc92d
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-20 15:52:29 +0000

    Added categories as a reuseable component used by both suppliers and demand

---
+++ react/src/components/categoriescard.js
@@ -0,0 +1,26 @@
+'use client'
+
+import '@/styles/globals.css'
+import React, {useState, useEffect} from 'react'
+
+export default function CategoriesCard(props) {
+    const [categories, setCategories] = useState(null);
+    
+    useEffect(
+        () => {      
+            console.log("Loading categories, using URL "+props.categoriesUrl);
+            fetch(props.categoriesUrl)
+                .then(res => res.json())
+                .then((c) => setCategories(c)
+                );
+            
+    ***REMOVED***, []
+    );
+
+    if(categories === null) return <p>NULL</p>;
+    else return (
+        <div className="card">
+            {categories._embedded.categories.map( (c) => <div>{c.name}</div>)}
+        </div>
+    );
+}
--- react/src/components/linkdemandcard.js
+++ react/src/components/linkdemandcard.js
@@ -1,17 +1,20 @@
 'use client'
 
-import React, {useState, useEffect} from 'react'
 import '@/styles/globals.css'
+import CategoriesCard from '@/components/categoriescard'
 
 export default function LinkDemandCard({linkdemand}) {
     
     if(linkdemand === null) return <p>NULL</p>;
-    else return (
-        <div className="card">
-            <div>{linkdemand.name}</div>
-            <div>{linkdemand.url}</div>
-            <div>Domain Authority needed {linkdemand.daNeeded}</div>
-            <div>Requested {linkdemand.requested}</div>
-        </div>
-    );
+    else {
+        return (
+            <div className="card">
+                <div>{linkdemand.name}</div>
+                <div>{linkdemand.url}</div>
+                <div>Domain Authority needed {linkdemand.daNeeded}</div>
+                <div>Requested {linkdemand.requested}</div>
+                <CategoriesCard categoriesUrl={linkdemand._links.categories.href}/>
+            </div>
+        );
+***REMOVED***
 }
