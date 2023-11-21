fb660dde76f4e735cbedf5f44857fba0fccdaee4
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-21 10:54:13 +0000

    Added icons

--- react/src/components/suppliercard.js
+++ react/src/components/suppliercard.js
@@ -1,10 +1,14 @@
 'use client'
 
 import CategoriesCard from '@/components/categoriescard'
+import Image from 'next/image'
+import Icon from '@/components/shipping.png'
 
 export default function SupplierCard({supplier}) {
     return (
         <div className="list-card card">
+            
+            <Image src={Icon} width={32} height={32} alt="Shipping icon"/>
             <div>{supplier.name}</div>
             <div>DA: {supplier.da}</div>
             <div>Website: {supplier.website}</div>
