e85145c2e14e7df7cf987ae7a856b4548ef0c333
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-21 17:23:48 +0000

    CORS tweaks

--- src/main/java/com/domloge/slinkylinky/linkservice/repo/CategoryRepo.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/repo/CategoryRepo.java
@@ -2,10 +2,12 @@ package com.domloge.slinkylinky.linkservice.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.domloge.slinkylinky.linkservice.entity.Category;
 
 @RepositoryRestResource(collectionResourceRel = "categories", path = "categories")
+@CrossOrigin(originPatterns = {"*localhost*"})
 public interface CategoryRepo extends CrudRepository <Category, Long> {
     
     Category findByName(String name);
--- src/main/java/com/domloge/slinkylinky/linkservice/repo/PaidLinkRepo.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/repo/PaidLinkRepo.java
@@ -2,10 +2,12 @@ package com.domloge.slinkylinky.linkservice.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.domloge.slinkylinky.linkservice.entity.PaidLink;
 
 @RepositoryRestResource(collectionResourceRel = "paidlinks", path = "paidlinks")
+@CrossOrigin(originPatterns = {"*localhost*"})
 public interface PaidLinkRepo extends CrudRepository <PaidLink, Long> {
     
     // void createByBlogger_IdAndClient_Id(int logger_id, int client_id);
