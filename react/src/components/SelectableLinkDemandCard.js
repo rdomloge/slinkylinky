20aea057fc77bde43177c6d1ec4b6ecf24f26ec8
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-24 00:13:14 +0000

    First stab at the code for posting the form

---
+++ react/src/components/SelectableLinkDemandCard.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+
+export default function SelectableLinkDemandCard({children, onSelectedHandler, onDeselectedHandler}) {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => { 
+        setChecked(!checked); //this doesn't actually happen until the method completes
+        
+        if(!checked) onSelectedHandler();
+        else onDeselectedHandler();
+    }; 
+
+    return (
+        <>
+            <div className="grid grid-cols-10 rounded-lg bg-stone-300 mb-1 mr-2">
+                <div className="col-span-9">
+                    {children}
+                </div>
+                <input type="checkbox" id="" defaultValue={false} className="mr-1" onChange={handleChange}/> 
+            </div>
+        </>
+    );
+}
