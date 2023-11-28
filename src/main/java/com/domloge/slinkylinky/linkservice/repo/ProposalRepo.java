d15a7c840d8965d8880364c9bf49e15c54d59a80
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-28 10:08:37 +0000

    Added a new search

--- src/main/java/com/domloge/slinkylinky/linkservice/repo/ProposalRepo.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/repo/ProposalRepo.java
@@ -1,8 +1,11 @@
 package com.domloge.slinkylinky.linkservice.repo;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.data.repository.CrudRepository;
 
 import com.domloge.slinkylinky.linkservice.entity.Proposal;
 
@@ -10,4 +13,5 @@ import com.domloge.slinkylinky.linkservice.entity.Proposal;
 @CrossOrigin(exposedHeaders = "*")
 public interface ProposalRepo extends CrudRepository <Proposal, Long> {
     
+    List<Proposal> findAllByDateCreatedLessThanEqualAndDateCreatedGreaterThanEqual(LocalDateTime endDate, LocalDateTime startDate);
 }
