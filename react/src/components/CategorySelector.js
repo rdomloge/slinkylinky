afaa9cba529a6c1fca33b6ea1b5e48f8f52cb4b9
Ramsay.Domloge <rdomloge@gmail.com>
2023-11-28 16:31:32 +0000

    Some changes to the initial version where you can add link demand

--- react/src/components/CategorySelector.js
+++ react/src/components/CategorySelector.js
@@ -2,39 +2,20 @@
 import AsyncSelect from 'react-select/async';
 import React, {useState, useEffect} from 'react'
 
-export default function CategorySelector() {
-    // const [categories, setCategories] = useState();
+export default function CategorySelector({changeHandler}) {
     
-    // useEffect(
-    //     () => {      
-    //         fetch("http://localhost:8080/categories")
-    //             .then(res => res.json())
-    //             .then((json) => {
-    //                 const cats = json._embedded.categories.map(c => ({value: c.name, label: c.name}));
-    //                 setCategories(cats)
-    //                 console.log(JSON.stringify(cats));
-    //                 });
-            
-    //     }, []
-    // );
   
-    const handleChange = (selectedOption) => {
-        console.log("Selected option: "+selectedOption)
-    }
-
     const findCategories = (inputValue, callback) => {
-        console.log("FN invoked")
         fetch("http://localhost:8080/categories")
             .then(res => res.json())
             .then(
-                json => callback(json._embedded.categories.map(c => ({ value: c.name, label: c.name })))
+                json => callback(json._embedded.categories.map(c => ({ value: c._links.self.href, label: c.name })))
             )
     }
 
     return (
         <AsyncSelect
-            inputId='categoriesInput'
-            onChange={handleChange}
+            onChange={changeHandler}
             defaultOptions
             cacheOptions
             closeMenuOnSelect={false}
