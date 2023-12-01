c3f2c56862bdd26e0fe17423d73aaeccac1bfddf
Ramsay.Domloge <rdomloge@gmail.com>
2023-12-01 23:24:02 +0000

    Changes for using next-auth for login

--- react/next.config.js
+++ react/next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/demand",
+                permanent: true
+        ***REMOVED***
+        ]
+***REMOVED***,
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+            port: '',
+            pathname: '/a/**',
+      ***REMOVED***,
+        ],
+  ***REMOVED***,
+}
 
 module.exports = nextConfig
