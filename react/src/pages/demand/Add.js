ce7148ac957ca4c4bc8b38d3fbe00be4bd8d5318
Ramsay.Domloge <rdomloge@gmail.com>
2023-12-04 22:53:16 +0000

    Re-wrote to use the components for consistent L&F

--- react/src/pages/demand/Add.js
+++ react/src/pages/demand/Add.js
@@ -4,6 +4,8 @@ import Layout from "@/components/layout";
 import PageTitle from "@/components/pagetitle";
 import { useSession } from "next-auth/react";
 import CategoryFilter from '@/components/CategorySelector';
+import TextInput from '@/components/TextInput';
+import NumberInput from '@/components/NumberInput';
 
 export default function NewDemand() {
 
@@ -39,9 +41,9 @@ export default function NewDemand() {
     return (
         <Layout>
             <PageTitle title="New demand"/>
-            <div className="content-center">
+            <div className="content-center mb-4">
                 <button id="createnew" onClick={submitHandler} 
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 p-4 border border-blue-700 rounded">
                     Submit
                 </button>
             </div>
@@ -49,44 +51,26 @@ export default function NewDemand() {
             <form className="w-full max-w-lg card">
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-name">
-                            Name
-                        </label>
-                        <input onChange={(e)=>demand.name=e.target.value}
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" type="text"/>
-                        <p className="text-red-500 text-xs italic">Required</p>
+                        <TextInput label="name" changeHandler={(e)=>demand.name=e}/>
                     </div>
                     <div className="w-full md:w-1/2 px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-anchor-text">
-                            Anchor text
-                        </label>
-                        <input onChange={(e)=>demand.anchorText=e.target.value} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-anchor-text" type="text"/>
+                        <TextInput label="Anchor text" changeHandler={(e)=>demand.anchorText=e} />
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-url">
-                            URL
-                        </label>
-                        <input onChange={(e)=>demand.url=e.target.value}
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-url" />
-                        <p className="text-gray-600 text-xs italic">We&apos;ll work out the domain from this</p>
+                        <TextInput label="URL" changeHandler={(e)=>demand.url=e} />
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-2">
                     <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-da-needed">
-                            DA needed
-                        </label>
-                        <input id="grid-da-needed" type="number" placeholder="DA" onChange={(e)=>demand.daNeeded=e.target.value}
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
-                        <p className="text-red-500 text-xs italic">Required</p>
+                        <NumberInput label="DA needed" changeHandler={(e)=>demand.daNeeded=e} />
                     </div>
                     <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
                         <CategorySelector changeHandler={categoryChangeHandler} label="Categories"/>
                     </div>
-                    <div className="w-full md:w-3/3 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
+                    <div className="w-full md:w-3/3 px-3 mb-6 mt-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
                             Requested
                         </label>
                         <div className="relative">
