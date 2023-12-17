8ecb275bd72b828948900d156e41e570babbb6a0
Ramsay.Domloge <rdomloge@gmail.com>
2023-12-17 15:33:11 +0000

    Added more attractive minimal category lite

--- react/src/components/category.js
+++ react/src/components/category.js
@@ -10,3 +10,11 @@ export default function Category({category}) {
         </div>
     );
 }
+
+export function CategoryLite({category}) {
+    return (
+        <span className="float-right bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 mb-1 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+            {category.name}
+        </span>
+    );
+}
