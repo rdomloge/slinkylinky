74ab7bac4cdda79f37c431b72dc89f7d0cc04938
Ramsay.Domloge <rdomloge@gmail.com>
2023-12-20 16:47:27 +0000

    Just backup things

--- restore-from-backup.sh
+++ restore-from-backup.sh
@@ -3,4 +3,4 @@
 # This assumes that the database you are restoring to already is running on IP 10.0.0.229 and there is a 
 # backup file in the Windows Download named 2023-12-12T10_24_59Z-backup.sql
 
-docker run --env PGPASSWORD=sl --entrypoint psql -v C:/Users/domloger/Downloads/temp:/backup -ti postgres -f backup/2023-12-12T10_24_59Z-backup.sql -h 10.0.0.229 -U slinkylinky slinkylinky
+docker run --env PGPASSWORD=sl --entrypoint psql -v C:/Users/domloger/Downloads/temp:/backup -ti postgres -f backup/2023-12-14_1400-backup.sql -h 10.0.0.229 -U slinkylinky slinkylinky
