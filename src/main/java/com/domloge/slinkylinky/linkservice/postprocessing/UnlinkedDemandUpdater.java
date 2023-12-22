b11743f19887d8f6239c016f87cae5eae0a4768b
Ramsay.Domloge <rdomloge@gmail.com>
2023-12-22 19:58:36 +0000

    Initial draft

---
+++ src/main/java/com/domloge/slinkylinky/linkservice/postprocessing/UnlinkedDemandUpdater.java
@@ -0,0 +1,42 @@
+package com.domloge.slinkylinky.linkservice.postprocessing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+
+import com.domloge.slinkylinky.linkservice.entity.Category;
+import com.domloge.slinkylinky.linkservice.entity.Demand;
+import com.domloge.slinkylinky.linkservice.entity.DemandSite;
+import com.domloge.slinkylinky.linkservice.repo.DemandRepo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RepositoryEventHandler(DemandSite.class)
+@Slf4j
+public class UnlinkedDemandUpdater {
+    
+    @Autowired
+    private DemandRepo demandRepo;
+
+    @HandleBeforeSave
+    public void handleAfterSave(DemandSite demandSite) {
+        if(null == demandSite) {
+            throw new IllegalStateException("demand site is null");
+        }
+
+        demandSite.getDemands().forEach(demand -> {
+            LinkedList<Category> newCats = new LinkedList<>();
+            demandSite.getCategories().forEach(newCats::add);
+            demand.setCategories(newCats);
+            // what do we do if the demand site domain no longer matches the demand domain?
+            if ( ! demand.getDomain().equals(demandSite.getDomain())) {
+                log.warn("Demand site domain {} no longer matches demand domain {}", demandSite.getDomain(), demand.getDomain());                
+            }
+            demandRepo.save(demand);
+        });
+    }
+}
