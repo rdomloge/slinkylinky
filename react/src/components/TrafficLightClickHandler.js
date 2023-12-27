212a0d5de36b97d2339c7ec44c1217b8aa5477d2
Ramsay.Domloge <rdomloge@gmail.com>
2023-12-27 01:48:00 +0000

    Support for 3rd party suppliers

--- react/src/components/TrafficLightClickHandler.js
+++ react/src/components/TrafficLightClickHandler.js
@@ -5,12 +5,16 @@ import React, {useState} from 'react'
 import Modal from "./atoms/Modal";
 import TextInput from "./atoms/TextInput";
 import { ClickHandlerButton } from "./atoms/Button";
+import NumberInput from "./atoms/NumberInput";
 
 export default function TrafficLightClickHandler({children, proposal, updateHandler, propertyName, propertyDate}) {
     const { data: session } = useSession();
     const [showModal, setShowModal] = useState(false)
     const [postTitle, setPostTitle] = useState("")
     const [postUrl, setPostUrl] = useState("")
+    const [supplierCost, setSupplierCost] = useState(0)
+    const [supplierDa, setSupplierDa] = useState(0)
+    const [supplierCurrency, setSupplierCurrency] = useState()
 
     function parseId(entity) {
         const url = entity._links.self.href;
@@ -32,9 +36,17 @@ export default function TrafficLightClickHandler({children, proposal, updateHand
             toggle();
 ***REMOVED***
 
-    function setCapturedValue(url, title) {
+    function setCapturedValue(url, title, supplierCost, supplierDa) {
         proposal.liveLinkUrl = url;
         proposal.liveLinkTitle = title;
+
+        if(proposal.paidLinks[0].supplier.thirdParty) {
+            proposal.paidLinks[0].supplier.weWriteFee = supplierCost
+            proposal.paidLinks[0].supplier.weWriteFeeCurrency = supplierCurrency
+            proposal.paidLinks[0].supplier.da = supplierDa
+            proposal.paidLinks[0].supplier.website = stripUrlPath(url)
+    ***REMOVED***
+
         if("" === url || "" === title) 
             console.log("Blank URL or title");
         else
@@ -60,36 +72,99 @@ export default function TrafficLightClickHandler({children, proposal, updateHand
             headers: {'Content-Type':'application/json'},
             body: bodyJson
     ***REMOVED***).then( (resp) => {
-            proposal[propertyName] = ! proposal[propertyName]
-            if(null != propertyDate) {
-                proposal[propertyDate] = proposal[propertyName] ? new Date().toISOString() : null
+            if(resp.ok) {
+                proposal[propertyName] = ! proposal[propertyName]
+                if(null != propertyDate) {
+                    proposal[propertyDate] = proposal[propertyName] ? new Date().toISOString() : null
+            ***REMOVED***
+
+                if("blogLive" === propertyName) {
+                    const supplierUrl = "/.rest/suppliers/"+proposal.paidLinks[0].supplier.id;
+                    fetch(supplierUrl, {
+                        method: 'PATCH',
+                        headers: {'Content-Type':'application/json'},
+                        body: JSON.stringify(create3rdPartySupplierPatchData())
+                ***REMOVED***).then( (resp) => {
+                        if(resp.ok) {
+                            updateHandler({...proposal***REMOVED*** // force a re-render
+                    ***REMOVED***
+                ***REMOVED***)
+                    .catch(error => console.error("Oh noes! An error: "+error));
+            ***REMOVED***
+                else {
+                    updateHandler({...proposal***REMOVED*** // force a re-render, since we don't need to update the supplier
+            ***REMOVED***
         ***REMOVED***
-            updateHandler({...proposal***REMOVED***
     ***REMOVED***).catch(error => console.error("Oh noes! An error: "+error));
+***REMOVED***
+
+    function stripUrlPath(url) {
+        if( ! url.startsWith("http")) {
+            url = "https://"+url
+    ***REMOVED***
+        const urlObj = new URL(url)
+        return urlObj.origin
+***REMOVED***
+
+    function create3rdPartySupplierPatchData() {
+        const clearingData = proposal[propertyDate] === null; // clear the supplier data if the post is no longer live
+        if(clearingData) {
+            setSupplierCost(null)
+            setSupplierDa(null)
+            setSupplierCurrency(null)
+            proposal.paidLinks[0].supplier.weWriteFee = null
+            proposal.paidLinks[0].supplier.weWriteFeeCurrency = null
+            proposal.paidLinks[0].supplier.da = null
+            proposal.paidLinks[0].supplier.website = null
+            proposal.paidLinks[0].supplier.domain = null
+    ***REMOVED***
+
+        const supplierData = {}
+        supplierData.updatedBy = session.user.email
+        supplierData.weWriteFee = clearingData ? null : supplierCost
+        supplierData.weWriteFeeCurrency = clearingData ? null : supplierCurrency
+        supplierData.da = clearingData ? null : supplierDa
+        supplierData.website = clearingData ? null : stripUrlPath(proposal.liveLinkUrl)
+        return supplierData
 ***REMOVED***
 
     return (
         <div className="inline-block cursor-pointer">
-        <div onClick={() => clickHandler()}>
-            {children}
-        </div>
-        <div>
-            {showModal ?
-                <Modal dismissHandler={()=>setShowModal(false)} title="Post details" width="w-1/3">
-                    <TextInput label="Post titlë" changeHandler={(e)=>setPostTitle(e)}/>
-                    <TextInput label="Live link URL" changeHandler={(e)=>setPostUrl(e)}/>
-                    <div className="pt-4">
-                        <ClickHandlerButton label="Submit" 
-                            clickHandler={()=> {
-                                setCapturedValue(postUrl,postTitle);
-                                setShowModal(false);
-                         ***REMOVED***}/>
-                    </div>
-                </Modal>
-                :
-                null
-        ***REMOVED***
-        </div>
+            <div onClick={() => clickHandler()}>
+                {children}
+            </div>
+            <div>
+                {showModal ?
+                    <Modal dismissHandler={()=>setShowModal(false)} title="Post details" width="w-1/3">
+                        <TextInput label="Post title" changeHandler={(e)=>setPostTitle(e)}/>
+                        <TextInput label="Live link URL" changeHandler={(e)=>setPostUrl(e)}/>
+                        {proposal.paidLinks[0].supplier.thirdParty ?
+                            <>
+                            <div className="w-1/3 inline-block p-4">
+                                <TextInput label="3rd party currency" changeHandler={(e)=>setSupplierCurrency(e)}/>
+                            </div>
+                            <div className="w-1/3 inline-block p-4">
+                                <NumberInput label="3rd party cost" changeHandler={(e)=>setSupplierCost(e)}/>
+                            </div>
+                            <div className="w-1/3 inline-block p-4">
+                                <NumberInput label="3rd party DA" changeHandler={(e)=>setSupplierDa(e)}/>
+                            </div>
+                            </>
+                        :
+                            null
+                    ***REMOVED***
+                        <div className="pt-4">
+                            <ClickHandlerButton label="Submit" 
+                                clickHandler={()=> {
+                                    setCapturedValue(postUrl,postTitle, supplierCost, supplierDa);
+                                    setShowModal(false);
+                            ***REMOVED***}/>
+                        </div>
+                    </Modal>
+                    :
+                    null
+            ***REMOVED***
+            </div>
         </div>
     );
 }
--- react/src/pages/supplier/search/[demandid].js
+++ react/src/pages/supplier/search/[demandid].js
@@ -1,17 +1,27 @@
 'use client'
 
+import { useSession } from "next-auth/react";
+
 import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import DemandCard from '@/components/demandcard'
 import SupplierCard from '@/components/suppliercard'
 import PageTitle from '@/components/pagetitle'
 import Layout from '@/components/Layout'
+import SessionButton, { ClickHandlerButton } from '@/components/atoms/Button'
+import Modal from '@/components/atoms/Modal'
+import TextInput from '@/components/atoms/TextInput'
+import { headers } from "../../../../next.config";
 
 export default function App() {
     const router = useRouter()
+    const { data: session } = useSession();
+
     const [demand, setDemand] = useState(null);
     const [suppliers, setSuppliers] = useState(null);
     const [existingLinkCount, setExistingLinkCount] = useState()
+    const [showModal, setShowModal] = useState(false);
+    const [newSupplierName, setNewSupplierName] = useState();
 
     useEffect(
         () => {
@@ -36,6 +46,82 @@ export default function App() {
     ***REMOVED***, [router.isReady, router.query.demandid]
     );
 
+    function build3rdPartySupplier() {   
+        const postData = {}
+        postData.thirdParty = true
+        postData.createdBy = session.user.email
+        postData.name = newSupplierName
+        return postData
+***REMOVED***
+
+    function buildPaidLink(newSupplierLocation) {
+        
+        const plData = {
+            "supplier": newSupplierLocation,
+            "demand": "/demands/"+demand.id
+    ***REMOVED***
+        return plData
+***REMOVED***
+
+    function buildProposal(plLocation) {
+        const plLocations = [plLocation]
+        const pData = {
+            "paidLinks": plLocations,
+            "dateCreated": new Date().toISOString(),
+            "createdBy": session.user.email
+    ***REMOVED***
+        return pData
+***REMOVED***
+
+    function create3rdPartyProposal() {
+        console.log("Creating 3rd party Supplier: "+newSupplierName);
+        setShowModal(false);
+        
+        const supplierUrl = "/.rest/suppliers"
+        fetch(supplierUrl, {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(build3rdPartySupplier())
+    ***REMOVED***)
+        .then( (resp) => {
+            if(resp.ok) {
+                const paidlinkUrl = "/.rest/paidlinks";
+                return fetch(paidlinkUrl, {
+                    method: 'POST',
+                    headers: {'Content-Type':'application/json'},
+                    body: JSON.stringify(buildPaidLink(resp.headers.get('Location')))
+             ***REMOVED***)
+        ***REMOVED***
+            else {
+                console.log("Created 3rd party supplier failed: "+JSON.stringify(resp));
+        ***REMOVED***
+    ***REMOVED***)
+        .then( (resp) => {  
+            if(resp.ok) {
+                const proposalUrl = "/.rest/proposals";
+                return fetch(proposalUrl, {
+                    method: 'POST',
+                    headers: {'Content-Type':'application/json'},
+                    body: JSON.stringify(buildProposal(resp.headers.get('Location')))
+            ***REMOVED***)
+        ***REMOVED***
+            else {
+                console.log("Created paidlink failed: "+JSON.stringify(resp));
+        ***REMOVED***
+    ***REMOVED***)
+        .then( (resp) => {
+            if(resp.ok) {
+                console.log("Created proposal");
+                const locationUrl = resp.headers.get('Location')
+                location.href = "/proposals/"+locationUrl.substring(locationUrl.lastIndexOf('/')+1);
+        ***REMOVED***
+            else {
+                console.log("Created proposal failed: "+JSON.stringify(resp));
+        ***REMOVED***
+    ***REMOVED***)
+***REMOVED***
+
+
     return (
         <Layout>
             <PageTitle title="Find a matching supplier"/>
@@ -43,8 +129,25 @@ export default function App() {
                 <div className='flex-1'>
                     <DemandCard demand={demand} />
                 </div>
-                <div className='card flex-none text-lg font-bold m-4 text-zinc-600'>There are <span className='text-5xl'>{existingLinkCount}</span> historical links tracked for this domain</div>
+                <div className='card flex-none text-lg font-bold m-4 text-zinc-600'>
+                    There are 
+                    <span className='text-5xl'> {existingLinkCount} </span> 
+                    historical links tracked for this domain
+                    <div className='mt-4'>
+                        <SessionButton label="Use 3rd party" clickHandler={(e) => setShowModal(true)}/>
+                    </div>
+                </div>
             </div>
+            {showModal ?
+                <Modal dismissHandler={()=>setShowModal(false)} title="3rd Party options" >
+                    <TextInput label="Supplier name" changeHandler={(e)=>setNewSupplierName(e)}/>
+                    <div className='mt-4'>
+                        <ClickHandlerButton label="Create" clickHandler={()=>create3rdPartyProposal()}/>
+                    </div>
+                </Modal>
+            : 
+                null
+        ***REMOVED***
             <div>Matching suppliers</div>
             <SupplierList suppliers={suppliers} demand={demand} demandid={router.query.demandid}/>
         </Layout>
