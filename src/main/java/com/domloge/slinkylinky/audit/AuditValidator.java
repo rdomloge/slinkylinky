c7829ecc00d9f50b24c1a0494b2bd19150086281
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-12 11:13:35 +0000

    first commit

---
+++ src/main/java/com/domloge/slinkylinky/audit/AuditValidator.java
@@ -0,0 +1,24 @@
+package com.domloge.slinkylinky.audit;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component("beforeCreateAuditRecordValidator")
+public class AuditValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return AuditRecord.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        AuditRecord ar = (AuditRecord) target;
+        if(null == ar.getDetail()) errors.rejectValue("detail", "missing");
+        if(null == ar.getEventTime()) errors.rejectValue("eventTime", "missing");
+        if(null == ar.getWhat()) errors.rejectValue("what", "missing");
+        if(null == ar.getWho() || ar.getWho().trim().isEmpty()) errors.rejectValue("who", "missing");
+        if(null == ar.getEntityType()) errors.rejectValue("entityType", "missing");
+    }
+}
