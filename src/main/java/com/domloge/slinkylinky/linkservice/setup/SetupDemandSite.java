4b729db78dce225bbbfd2b6ece394fd3c6a04d15
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-12 11:10:33 +0000

    Add null checks for category fields in SetupDemandSite class

--- src/main/java/com/domloge/slinkylinky/linkservice/setup/SetupDemandSite.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/setup/SetupDemandSite.java
@@ -11,4 +11,32 @@ public class SetupDemandSite {
     private String website;
 
     private String category1, category2, category3, category4;
+
+    public String getCategory1() {
+        if(category1.isEmpty()) {
+            return null;
+        }
+        return category1;
+    }
+
+    public String getCategory2() {
+        if(category2.isEmpty()) {
+            return null;
+        }
+        return category2;
+    }
+
+    public String getCategory3() {
+        if(category3.isEmpty()) {
+            return null;
+        }
+        return category3;
+    }
+
+    public String getCategory4() {
+        if(category4.isEmpty()) {
+            return null;
+        }
+        return category4;
+    }
 }
