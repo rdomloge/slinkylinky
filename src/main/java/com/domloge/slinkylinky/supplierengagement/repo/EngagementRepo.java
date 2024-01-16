c3c22294d482e955dbcb523b04c9271b5dcb13b3
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-16 15:18:10 +0000

    Add JPA repository annotations and import statements

--- src/main/java/com/domloge/slinkylinky/supplierengagement/repo/EngagementRepo.java
+++ src/main/java/com/domloge/slinkylinky/supplierengagement/repo/EngagementRepo.java
@@ -1,8 +1,13 @@
 package com.domloge.slinkylinky.supplierengagement.repo;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.domloge.slinkylinky.supplierengagement.entity.Engagement;
 
@@ -14,5 +19,8 @@ public interface EngagementRepo extends CrudRepository <Engagement, Long> {
     
     @Transactional
     public Engagement findByGuid(String guid);
+
+    
+    
 
 }
