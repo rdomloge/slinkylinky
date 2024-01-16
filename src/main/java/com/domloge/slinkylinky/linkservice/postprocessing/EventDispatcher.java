efa27eeb7ade2294adf235fef86d5f9f03739d5e
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-16 15:24:29 +0000

    Refactor EventDispatcher to include Supplier details in ProposalUpdateEvent

--- src/main/java/com/domloge/slinkylinky/linkservice/postprocessing/EventDispatcher.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/postprocessing/EventDispatcher.java
@@ -6,16 +6,16 @@ import org.springframework.data.rest.core.annotation.HandleAfterSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
 
+import com.domloge.slinkylinky.events.ProposalUpdateEvent;
+import com.domloge.slinkylinky.events.ProposalUpdateEvent.ProposalEventType;
 import com.domloge.slinkylinky.linkservice.entity.Proposal;
+import com.domloge.slinkylinky.linkservice.entity.Supplier;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -26,18 +26,7 @@ public class EventDispatcher {
     @Autowired
     private AmqpTemplate proposalsRabbitTemplate;
 
-    enum ProposalEventType {
-        CREATED, UPDATED, DELETED
-    }
-
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    class ProposalEvent {
-
-        private ProposalEventType type;
-        private Proposal proposal;
-    }
+    
  
     @HandleAfterSave
     public void handleAfterSave(Proposal proposal) throws JsonProcessingException {
@@ -47,6 +36,11 @@ public class EventDispatcher {
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.setSerializationInclusion(Include.NON_NULL);
 
-        proposalsRabbitTemplate.convertAndSend(new ProposalEvent(ProposalEventType.UPDATED, proposal));
+        Supplier supplier = proposal.getPaidLinks().get(0).getSupplier();
+        ProposalUpdateEvent event = new ProposalUpdateEvent(ProposalEventType.UPDATED);
+        event.setProposalDetails(proposal.getArticle(), proposal.getId());
+        event.setSupplierDetails(supplier.getName(), supplier.getEmail(), supplier.getWebsite(), supplier.getWeWriteFee(), 
+            supplier.getWeWriteFeeCurrency(), supplier.isThirdParty());
+        proposalsRabbitTemplate.convertAndSend(event);
     }
 }
