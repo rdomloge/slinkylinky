2a38fe74c9529e94fdfa25f76e0feb005c108b40
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-16 15:13:45 +0000

    Removed annotations since the entity is now in the audit MS

--- src/main/java/com/domloge/slinkylinky/supplierengagement/AuditRecord.java
+++ src/main/java/com/domloge/slinkylinky/supplierengagement/AuditRecord.java
@@ -2,24 +2,14 @@ package com.domloge.slinkylinky.supplierengagement;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
 @Getter 
 @Setter
 @ToString
 public class AuditRecord {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
 
     private Long entityId;
 
@@ -31,8 +21,5 @@ public class AuditRecord {
 
     private LocalDateTime eventTime;
 
-    @Lob
-    @Column(columnDefinition="TEXT")
     private String detail;
-
 }
