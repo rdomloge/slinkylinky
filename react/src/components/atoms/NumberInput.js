79cdb434e04a112de1b00c0784cef136f6f69c37
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-16 15:37:39 +0000

    Add disabled prop to NumberInput component

--- react/src/components/atoms/NumberInput.js
+++ react/src/components/atoms/NumberInput.js
@@ -1,13 +1,13 @@
 
 
-export default function NumberInput({changeHandler, label, initialValue}) {
+export default function NumberInput({changeHandler, label, initialValue, disabled}) {
 
     return (
         <div className="w-full px-3 mb-6 md:mb-0 border-b border-teal-500">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-name">
                 {label}
             </label>
-            <input onChange={(e)=>changeHandler(e.target.value)} defaultValue={initialValue} id="grid-name" type="number"
+            <input onChange={(e)=>changeHandler(e.target.value)} defaultValue={initialValue} id="grid-name" type="number" disabled={disabled}
                 className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"/>
         </div>
     );
