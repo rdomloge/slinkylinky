f99e56da56cf4888a900deea6dcbf24051a0d83c
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-16 15:14:42 +0000

    Switched from JSON objects to events from the event lib

--- src/main/java/com/domloge/slinkylinky/supplierengagement/ProposalEventReceiver.java
+++ src/main/java/com/domloge/slinkylinky/supplierengagement/ProposalEventReceiver.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Component;
 
+import com.domloge.slinkylinky.events.ProposalUpdateEvent;
 import com.domloge.slinkylinky.supplierengagement.email.Context;
 import com.domloge.slinkylinky.supplierengagement.email.EmailBuilder;
 import com.domloge.slinkylinky.supplierengagement.email.EmailSender;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -31,29 +35,31 @@ public class ProposalEventReceiver {
     @Autowired
     private AmqpTemplate auditRabbitTemplate;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     
-    public void receiveMessage(String message) throws AddressException, MessagingException {
-        JsonObject json = JsonParser.parseString(message).getAsJsonObject();
-        if(!json.has("type")) {
-            log.warn("No type in message");
+    public void receiveMessage(String message) throws AddressException, MessagingException, JsonMappingException, JsonProcessingException {
+        log.info("Received <" + message + ">");
+        
+        ProposalUpdateEvent event = mapper.readValue(message, ProposalUpdateEvent.class);
+        if(event.isSupplierIs3rdParty()) {
+            log.info("Supplier is 3rd party, not sending email");
             return;
         }
-        String type = json.get("type").getAsString();
-        log.info("Received <" + message + ">");
-
-        if(json.has("proposal") && json.getAsJsonObject("proposal").has("article")) {
-            JsonObject proposal = json.getAsJsonObject("proposal");
-            String article = proposal.get("article").getAsString();
+        
+        if(event.getArticle() != null) {
+            String article = event.getArticle();
             log.info("Article: " + article);
             // let's assume that this is the first time we see this article
             // and that we want to send an email to the supplier
-            Context ctx = emailBuilder.build(article, proposal);
+            Context ctx = emailBuilder.build(article, event);
             MimeMessage mimeMessage = ctx.getMessage();
             try {
-                emailSender.send(mimeMessage);
+                emailSender.send(mimeMessage, event.getProposalId());
 
                 AuditRecord auditRecord = new AuditRecord();
-                auditRecord.setEntityId(proposal.get("id").getAsLong());
+                auditRecord.setEntityId(event.getProposalId());
                 auditRecord.setEntityType("Proposal");
                 auditRecord.setEventTime(java.time.LocalDateTime.now());
                 auditRecord.setWho("haven't worked this out yet");
@@ -64,14 +70,13 @@ public class ProposalEventReceiver {
             catch(MailException e) {
                 log.error("Failed to send email", e);
             }
-            
         }
         else {
-            log.warn("No article in proposal");
+            log.info("No article in event");
         }
     }
 
-    public void receiveMessage(byte[] message) throws AddressException, MessagingException {
+    public void receiveMessage(byte[] message) throws AddressException, MessagingException, JsonMappingException, JsonProcessingException {
         try {
             receiveMessage(new String(message, "utf-8"));
         } 
