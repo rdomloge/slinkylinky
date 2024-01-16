bd570c1fdb97d61feea0d83127279ee009e335ac
Ramsay.Domloge <rdomloge@gmail.com>
2024-01-16 15:39:49 +0000

    Fix Docker bind mount path in run_dev.sh

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-docker run --restart unless-stopped --mount type=bind,source="${PWD}"/react,target=/adminwebsite -p 3000:3000 --name react -ti node:current-slim bash
+docker run --restart unless-stopped --mount type=bind,source="${PWD}/react",target=/adminwebsite -p 3000:3000 --name react -ti node:current-slim bash
 
-echo Now, npm run dev
+# Now, npm run dev
