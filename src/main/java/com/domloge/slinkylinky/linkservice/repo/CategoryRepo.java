85cc9af09287d909ee42e16173bfd1c7b366d1c1
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 12:00:45 +0000

    Add new methods to CategoryRepo.java

--- src/main/java/com/domloge/slinkylinky/linkservice/repo/CategoryRepo.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/repo/CategoryRepo.java
@@ -2,6 +2,7 @@ package com.domloge.slinkylinky.linkservice.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.domloge.slinkylinky.linkservice.entity.Category;
@@ -12,5 +13,34 @@ public interface CategoryRepo extends CrudRepository <Category, Long> {
     
     Category findByNameIgnoreCase(String name);
 
-    Category[] findByNameContainsIgnoreCase(String name);
+    Category[] findByNameContainsIgnoreCaseAndDisabledFalseOrderByNameAsc(String name); // used by the category selector (when searching)
+
+    Category[] findAllByDisabledFalseOrderByNameAsc(); // used by the category selector
+    
+    Category[] findAllByOrderByNameAsc(); // returns disabled categories too, for the category admin page
+
+
+
+        
+    @Override
+    @RestResource(exported = false)
+    void delete(Category entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Category> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Long> ids);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long id);
+
+    
 }
