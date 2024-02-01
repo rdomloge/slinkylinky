581374f43131c53486108baf51d9ee84e2328e95
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:24:59 +0000

    Refactor demand categories filter and input disabling

--- react/src/components/AddOrEditDemand.js
+++ react/src/components/AddOrEditDemand.js
@@ -13,6 +13,7 @@ export default function AddOrEditDemand({demand, updateNudge}) {
     const { data: session } = useSession();
     const [searchTerm, setSearchTerm] = useState()
     const [demandsite, setDemandsite] = useState()
+    const [editMode] = useState(demand.id ? true : false)
         
 
     function submitHandler() {
@@ -66,7 +67,7 @@ export default function AddOrEditDemand({demand, updateNudge}) {
         setDemandsite(demandsite)
         setSearchTerm(demandsite.name)
         demand.name = demandsite.name
-        demand.categories = demandsite.categories
+        demand.categories = demandsite.categories.filter(c => c.disabled == false)
         demand.url = demandsite.url
         updateNudge(demand)
 ***REMOVED***
@@ -92,26 +93,26 @@ export default function AddOrEditDemand({demand, updateNudge}) {
                             {demandsite ?
                                 <p className="h-full flex items-center text-xl">{demandsite.name}</p>
                             :
-                                <TextInput label="name" disabled={demandsite} changeHandler={(e)=>{
+                                <TextInput label="name" disabled={editMode || demandsite} changeHandler={(e)=>{
                                     demand.name=e
                                     setSearchTerm(e)
                             ***REMOVED***} initialValue={demand.name}/>
                         ***REMOVED***
                         </div>
                         <div className="w-full md:w-1/2 px-3">
-                            <TextInput label="Anchor text" changeHandler={(e)=>demand.anchorText=e} initialValue={demand.anchorText} disabled={!demandsite}/>
+                            <TextInput label="Anchor text" changeHandler={(e)=>demand.anchorText=e} initialValue={demand.anchorText} disabled={!demand.id && !demandsite}/>
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full px-3">
-                            <TextInput label="URL" changeHandler={(e)=>demand.url=e} initialValue={demand.url} disabled={!demandsite}/>
+                            <TextInput label="URL" changeHandler={(e)=>demand.url=e} initialValue={demand.url} disabled={!demand.id && !demandsite}/>
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-2">
-                        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-                            <NumberInput label="DA needed" changeHandler={(e)=>demand.daNeeded=e} initialValue={demand.daNeeded}  disabled={!demandsite}/>
+                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                            <NumberInput label="DA needed" changeHandler={(e)=>demand.daNeeded=e} initialValue={demand.daNeeded}  disabled={!demand.id && !demandsite}/>
                         </div>
-                        <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
+                        <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
                                 Categories
                             </label>
