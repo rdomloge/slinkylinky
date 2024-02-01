fc85e2e9bc1955316d15d23c17f78f6512ae4333
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:29:11 +0000

    Refactor ProposalTrafficLights component to work with 3rd party suppliers

--- react/src/components/ProposalTrafficLights.js
+++ react/src/components/ProposalTrafficLights.js
@@ -6,11 +6,21 @@ export default function TrafficLights({proposal, updateHandler, interactive}) {
     if( ! interactive) {
         return (
             <div className="p-2 m-2 h-13">
-                <TrafficLight text="Content" value={proposal.contentReady} />
+                { ! proposal.paidLinks[0].supplier.thirdParty ?
+                    <TrafficLight text="Content" value={proposal.contentReady} />
+                :
+                    null
+                }
                 <TrafficLight text="Sent" value={proposal.proposalSent} />
-                <TrafficLight text="Accepted" value={proposal.proposalAccepted} />
-                <TrafficLight text="Invoice recv" value={proposal.invoiceReceived} />
-                <TrafficLight text="Invoice paid" value={proposal.invoicePaid} />
+                { ! proposal.paidLinks[0].supplier.thirdParty ?
+                    <>
+                    <TrafficLight text="Accepted" value={proposal.proposalAccepted} />
+                    <TrafficLight text="Invoice recv" value={proposal.invoiceReceived} />
+                    <TrafficLight text="Invoice paid" value={proposal.invoicePaid} />
+                    </>
+                :
+                null
+                }
                 <TrafficLight text="Live" value={proposal.blogLive} />
             </div>
         );
@@ -18,21 +28,32 @@ export default function TrafficLights({proposal, updateHandler, interactive}) {
     else {
         return (
             <div className="p-2 m-2 h-13">
-                <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="contentReady">
-                    <TrafficLight text="Content" value={proposal.contentReady} />
-                </TrafficLightClickHandler>
+                { ! proposal.paidLinks[0].supplier.thirdParty ?
+                    <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="contentReady">
+                        <TrafficLight text="Content" value={proposal.contentReady} />
+                    </TrafficLightClickHandler>
+                : 
+                    null
+                }
                 <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="proposalSent" propertyDate="dateSentToSupplier">
                     <TrafficLight text="Sent" value={proposal.proposalSent} />
                 </TrafficLightClickHandler>
-                <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="proposalAccepted" propertyDate="dateAcceptedBySupplier">
-                    <TrafficLight text="Accepted" value={proposal.proposalAccepted} />
-                </TrafficLightClickHandler>
-                <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="invoiceReceived" propertyDate="dateInvoiceReceived">
-                    <TrafficLight text="Invoice recv" value={proposal.invoiceReceived} />
-                </TrafficLightClickHandler>
-                <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="invoicePaid" propertyDate="dateInvoicePaid">
-                    <TrafficLight text="Invoice paid" value={proposal.invoicePaid} />
-                </TrafficLightClickHandler>
+                { ! proposal.paidLinks[0].supplier.thirdParty ?
+                    <>
+                    <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="proposalAccepted" propertyDate="dateAcceptedBySupplier">
+                        <TrafficLight text="Accepted" value={proposal.proposalAccepted} />
+                    </TrafficLightClickHandler>
+                
+                    <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="invoiceReceived" propertyDate="dateInvoiceReceived">
+                        <TrafficLight text="Invoice recv" value={proposal.invoiceReceived} />
+                    </TrafficLightClickHandler>
+                    <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="invoicePaid" propertyDate="dateInvoicePaid">
+                        <TrafficLight text="Invoice paid" value={proposal.invoicePaid} />
+                    </TrafficLightClickHandler>
+                    </>
+                :
+                    null
+                }
                 <TrafficLightClickHandler proposal={proposal} updateHandler={updateHandler} propertyName="blogLive" propertyDate="dateBlogLive">
                     <TrafficLight text="Live" value={proposal.blogLive} />
                 </TrafficLightClickHandler>
