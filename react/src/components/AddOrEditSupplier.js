38dcea80c004cb8f3da3ca64370aaed16b1cd0c8
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:26:07 +0000

    Initial support for semrush

--- react/src/components/AddOrEditSupplier.js
+++ react/src/components/AddOrEditSupplier.js
@@ -10,13 +10,52 @@ import NumberInput from '@/components/atoms/NumberInput'
 import React, {useState} from 'react';
 import DisableToggle from './atoms/DisableToggle'
 import { fixForPosting } from "./CategoryUtil";
+import { StyledButton } from "./atoms/Button";
+import Modal from "./atoms/Modal";
+import SupplierSemRushTraffic from "./SupplierSemRushTraffic";
 
 export default function AddOrEditSupplier({supplier}) {
     const { data: session } = useSession();
     const [errorMsg, setErrorMsg] = useState()
+    const [showModal, setShowModal] = useState(false)
 
     function handleError(message) {
         setErrorMsg("Create failed: "+message)
+    }
+
+    function addProtocol(url) {
+        if (!/^(?:f|ht)tps?\:\/\//i.test(url)) {
+            url = "https://" + url;
+        }
+        return url;
+    }
+
+    function url_domain(data) {
+        let domain =  new URL(addProtocol(data))
+        let hostname = domain.hostname
+        return hostname.replace('www.','');
+    }
+
+    function lookupDa(domain) {
+        const url = "/.rest/mozsupport/checkdomain?domain="+domain
+        fetch(url, {method: 'GET', headers: {'user': session.user.email}})
+        .then( (resp) => {
+            if(resp.ok) {
+                resp.json().then( (data) => {
+                    supplier.da = data.domain_authority
+                })
+            }
+            else {
+                handleError("Unknown error: "+resp.status)
+            }
+        })
+        .catch(err => { handleError("Oops") });
+    }
+
+    function displaySemData() {
+        supplier.domain = url_domain(supplier.website)
+        lookupDa(supplier.domain)
+        setShowModal(true)
     }
     
     function submitHandler() {
@@ -83,41 +122,58 @@ export default function AddOrEditSupplier({supplier}) {
                     <DisableToggle changeHandler={(e) => supplier.disabled = e} initialValue={supplier.disabled}/>
                 </div>
                 <Image src={Icon} width={32} height={32} alt="Shipping icon"/>
-                <div className='w-1/3 inline-block pr-8'> 
-                    <TextInput initialValue={supplier.name} label={"Name"} changeHandler={(e)=>supplier.name = e}/> 
+
+
+                <div className='w-1/4 inline-block pr-8'> 
+                    <TextInput initialValue={supplier.name} label={"Name"} changeHandler={(e)=>supplier.name = e} /> 
                 </div>
                 <div className='w-28 inline-block pr-8'>
-                    <NumberInput initialValue={supplier.da} label={"DA"} changeHandler={(e)=>supplier.da = e}/>
+                    <NumberInput label={"DA"} changeHandler={(e)=>supplier.da = e} stateValue={supplier.da}/>
                 </div>
-                <div className='w-1/3 inline-block pr-8'>
+                <div className='w-1/4 inline-block pr-8'>
                     <TextInput initialValue={supplier.website} label={"Website"} changeHandler={(e)=>supplier.website = e}/> 
                 </div>
+                <div className='w-1/4 inline-block pr-8'>
+                    <TextInput initialValue={supplier.source} label={"Source"} changeHandler={(e)=>supplier.source = e}/> 
+                </div>
+
+
                 <div className='w-1/3 inline-block pr-8'>
                     <TextInput initialValue={supplier.email} label={"Email"} changeHandler={(e)=>supplier.email = e}/> 
+                </div>
+                
+                
+                <div className='w-20 inline-block pr-8 mt-8'>
+                    <TextInput initialValue="£" label="Currency" changeHandler={(e)=>supplier.weWriteFeeCurrency = e} maxLen={1}/>
                 </div>
                 <div className='w-28 inline-block pr-8'>
                     <NumberInput initialValue={supplier.weWriteFee} label={"Fee"} changeHandler={(e)=>supplier.weWriteFee = e}/>
                 </div>
-                <div className='w-28 inline-block pr-8 mt-8'>
-                    <NumberInput initialValue={supplier.semRushAuthorityScore} label={"SEM rush authority"} 
-                        changeHandler={(e)=>supplier.semRushAuthorityScore = e}/>
-                </div>
-                <div className='w-28 inline-block pr-8 mt-8'>
-                    <NumberInput initialValue={supplier.semRushUkMonthlyTraffic} label={"SEM rush UK monthly"} 
-                        changeHandler={(e)=>supplier.semRushUkMonthlyTraffic = e}/>
-                </div>
-                <div className='w-28 inline-block pr-8 mt-8'>
-                    <NumberInput initialValue={supplier.semRushUkJan23Traffic} label={"SEM rush UK Jan '23"} 
-                        changeHandler={(e)=>supplier.semRushUkJan23Traffic = e}/>
-                </div>
-                <div className='w-20 inline-block pr-8 mt-8'>
-                    <TextInput initialValue="£" label="Currency" changeHandler={(e)=>supplier.weWriteFeeCurrency = e} maxLen={1}/>
-                </div>
+
+                {supplier.id == null ?
+                    <div className="inline-block">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="srButton">
+                            Fetch DA and load SEM rush traffic (cost attached)
+                        </label>
+                        <StyledButton id="srButton" label="$ LOAD $" type="tertiary"
+                            submitHandler={()=>displaySemData()}  />
+                    </div>
+                :
+                    null
+                }
+
                 <div className='w-1/2 mt-8'>
                     <CategorySelector label="Categories"
                         changeHandler={(e)=> supplier.categories = e}
                         initialValue={supplier.categories}/>
                 </div>
+                {showModal ?
+                    <Modal title={"SEM rush traffic // "+supplier.domain} dismissHandler={()=>setShowModal(false)} width={"w-2/3"}>
+                        <SupplierSemRushTraffic supplier={supplier} adhoc={true}/>
+                    </Modal>
+                : 
+                    null
+                }
             </div>
             : <p>Loading supplier</p>}
         </>
