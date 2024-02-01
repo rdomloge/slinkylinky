25d92ff83eebd27fd993698a9bdc1da85cd483e1
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:36:21 +0000

    Add DotMenu, TextInput, and LabeledText components  and their respective states and functions

--- react/src/pages/proposals/[proposalid].js
+++ react/src/pages/proposals/[proposalid].js
@@ -13,6 +13,9 @@ import Loading from '@/components/Loading'
 import { StyledButton } from '@/components/atoms/Button'
 import Modal from '@/components/atoms/Modal'
 import { useSession } from 'next-auth/react'
+import DotMenu from '@/components/atoms/DotMenu'
+import TextInput from '@/components/atoms/TextInput'
+import LabeledText from '@/components/atoms/LabeledText'
 
 export default function Proposal() {
     const router = useRouter()
@@ -22,6 +25,10 @@ export default function Proposal() {
     const [error, setError] = useState(null)
     const [showAbortModal, setShowAbortModal] = useState(false)
     const { data: session } = useSession();
+    const [showInvoiceDownloadModal, setShowInvoiceDownloadModal] = useState(false);
+    const [engagement, setEngagement] = useState();
+    const [invoiceError, setInvoiceError] = useState(null);
+    const [showPreviewModal, setShowPreviewModal] = useState(false)
 
     useEffect(
         () => {
@@ -51,15 +58,46 @@ export default function Proposal() {
             .catch( (err) => setError(err));
 ***REMOVED***
 
+    function loadEngagement() {
+        const url = "/.rest/engagements/search/findByProposalIdAndStatusACCEPTED?proposalId="+router.query.proposalid;
+        fetch(url)
+            .then( (res) => res.json())
+            .then( (e) => setEngagement(e))
+            .catch( (err) => {
+                setInvoiceError(err.message)
+        ***REMOVED***);
+***REMOVED***
+
+    function createMenuItems() {
+        const items = [];
+        if(proposal) {
+            if(proposal.invoiceReceived) {
+                items.push({label: 'Download invoice', onClick: () => {
+                    setShowInvoiceDownloadModal(true)
+                    loadEngagement();
+            ***REMOVED******REMOVED***
+        ***REMOVED***
+            if(proposal.contentReady) {
+                items.push({label: 'View article', onClick: () => { 
+                    setShowPreviewModal(true);
+            ***REMOVED******REMOVED***
+        ***REMOVED***
+            items.push({label: 'Abort', onClick: () => setShowAbortModal(true)***REMOVED***
+    ***REMOVED***
+        return items;
+***REMOVED***
+
     return (
             <Layout>
+                
                 <PageTitle title="Proposal"/>
                 {proposal ?
                     <>
                     <div><NiceDate isostring={proposal.dateCreated}/></div>
                     <div className="float-right p-2 m-2">
-                        <StyledButton type='risky' label='Abort' submitHandler={() => setShowAbortModal(true)}/>
+                        
                     </div>
+                    <DotMenu items={createMenuItems()} classNames="float-left mt-6"/>
                     <TrafficLights proposal={proposal} updateHandler={setProposal} interactive={true}/>
                     {proposal.blogLive ?
                         <div className="card grid grid-cols-3 m-4">
@@ -71,7 +109,7 @@ export default function Proposal() {
                             </span>
                         </div>
                     : null}
-                    <div className="grid grid-cols-3">
+                    <div className="grid grid-cols-3 w-max">
                         <div>
                             <SupplierCard supplier={supplier}/>
                             <div className="card list-card">
@@ -100,6 +138,42 @@ export default function Proposal() {
                             </div>
                         </Modal>
                     : 
+                        null
+                ***REMOVED***
+                    {showInvoiceDownloadModal ?
+                        <Modal title={"Download invoice"} dismissHandler={() => setShowInvoiceDownloadModal(false)} width={"w-1/3"}>
+                            {engagement ?
+                                <>
+                                <LabeledText label="Filename" text={engagement.invoiceFileName} textClasses={"text-2xl"}/>
+                                <LabeledText label="File type" text={engagement.invoiceFileContentType} />
+                                <div className="flex justify-end pt-4">
+                                    <Link href={"/.rest/engagements/downloadInvoice?proposalId="+engagement.proposalId} target='_blank'>
+                                        Download
+                                    </Link>
+                                </div>
+                                </>
+                            : 
+                                null
+                        ***REMOVED***
+                            
+                            {invoiceError ?
+                                <>
+                                <p className='text-2xl font-dark py-2'>Error</p>
+                                <p className='font-dark pb-2'>{invoiceError}</p>
+                                </>
+                            : 
+                                null
+                        ***REMOVED***
+                            
+                        </Modal>
+                    : 
+                        null
+                ***REMOVED***
+                    {showPreviewModal ?
+                        <Modal title="Article" dismissHandler={() => { setShowPreviewModal(false) }} width="w-2/3">
+                            <iframe src={"/.rest/proposalsupport/getArticleFormatted?proposalId=" + router.query.proposalid} width="100%" height={480} />
+                        </Modal>
+                        :
                         null
                 ***REMOVED***
                     </>
