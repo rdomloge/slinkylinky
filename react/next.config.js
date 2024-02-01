ae0e3e8c56be28b9bcfe0080912a361c93f2ea01
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:24:08 +0000

    Update API endpoints in next.config.js

--- react/next.config.js
+++ react/next.config.js
@@ -26,15 +26,23 @@ const nextConfig = {
       return [
         {
           source: '/.rest/engagements/:path*',
-          destination: 'http://10.0.0.229:8081/.rest/engagements/:path*'
+          destination: 'http://host.docker.internal:8081/.rest/engagements/:path*'
     ***REMOVED***,
         {
           source: '/.rest/auditrecords/:path*',
-          destination: 'http://10.0.0.229:8082/.rest/auditrecords/:path*'
+          destination: 'http://host.docker.internal:8082/.rest/auditrecords/:path*'
+    ***REMOVED***,
+        {
+          source: '/.rest/stats/:path*',
+          destination: 'http://host.docker.internal:8083/.rest/stats/:path*'
+    ***REMOVED***,
+        {
+          source: '/.rest/semrush/:path*',
+          destination: 'http://host.docker.internal:8083/.rest/semrush/:path*'
     ***REMOVED***,
         {
           source: '/.rest/:path*',
-          destination: 'http://10.0.0.229:8080/.rest/:path*'
+          destination: 'http://host.docker.internal:8080/.rest/:path*'
     ***REMOVED***
       ]
 ***REMOVED***,
