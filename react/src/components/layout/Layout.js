8dd6074c79257ac0dd3a23ac97bb617a4fac9cfd
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:34:18 +0000

    Add Flowbite library and document file

--- react/src/components/layout/Layout.js
+++ react/src/components/layout/Layout.js
@@ -4,11 +4,15 @@ import React from 'react';
 import Menu from './Menu';
 import Footer from './Footer';
 import Header from './Header';
+import Script from 'next/script';
 
 const Layout =({children}) =>{
     
     return(
         <>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"/>
+            
+
             <Header/>
             
             <div className="grid grid-cols-8 gap-2">
