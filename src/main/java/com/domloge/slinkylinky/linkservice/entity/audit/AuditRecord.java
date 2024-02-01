922710d110bbde28e8277c4b0672e9954da03ee7
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:58:35 +0000

    Remove unnecessary annotations and fields from AuditRecord class

--- src/main/java/com/domloge/slinkylinky/linkservice/entity/audit/AuditRecord.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/entity/audit/AuditRecord.java
@@ -2,14 +2,6 @@ package com.domloge.slinkylinky.linkservice.entity.audit;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -18,7 +10,6 @@ import lombok.ToString;
 @Setter
 @ToString
 public class AuditRecord {
-    private long id;
 
     private Long entityId;
 
