8f5254e168eaa1d16a2fb49904dc739172c4fb7a
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:30:07 +0000

    Refactor Paging component to improve page number display

--- react/src/components/Paging.js
+++ react/src/components/Paging.js
@@ -1,7 +1,43 @@
+import React from 'react';
 export default function Paging({ total, page, pageCount, baseUrl }) {
-
     const pages = Array.from({ length: pageCount }, (_, i) => i + 1);
-    console.log("Page: "+page)
+
+    const getPageNumbers = () => {
+        if (pageCount <= 5) {
+            return pages;
+    ***REMOVED***
+
+        const pageNumbers = buildPageNumbers();
+
+        const uniquePageNumbers = Array.from(new Set(pageNumbers)).sort((a, b) => a - b);
+
+        if (uniquePageNumbers[0] !== 1) {
+            uniquePageNumbers.unshift("...");
+            uniquePageNumbers.unshift(1);
+    ***REMOVED***
+
+        if (uniquePageNumbers[uniquePageNumbers.length - 1] !== pageCount) {
+            uniquePageNumbers.push("...");
+            uniquePageNumbers.push(pageCount);
+    ***REMOVED***
+
+        return uniquePageNumbers;
+***REMOVED***;
+
+    function buildPageNumbers() {
+        const pageNumbers = [];
+        
+        const rangeStart = Math.min(pageCount - 4, Math.max(1, page - 2));
+        const rangeEnd = Math.min(pageCount, Math.max(page + 2 , rangeStart + 4));
+
+        for (let i = rangeStart; i <= rangeEnd; i++) {
+            pageNumbers.push(i);
+    ***REMOVED***
+
+
+        return pageNumbers;
+***REMOVED***
+
     return (
         <nav className="flex justify-center my-4">
             <button
@@ -11,9 +47,12 @@ export default function Paging({ total, page, pageCount, baseUrl }) {
             >
                 Previous
             </button>
-            {pages.map((pageNumber) => (
+            {getPageNumbers().map((pageNumber,index) => (
+                pageNumber === "..." ?
+                    <span className="px-3 py-2 mx-1 rounded-md" key={index}>...</span>
+                :
                 <button
-                    key={pageNumber}
+                    key={index}
                     onClick={() => window.location.href = `${baseUrl}?page=${pageNumber}`}
                     className={`px-3 py-2 mx-1 rounded-md ${pageNumber === page ? 'bg-blue-500 text-white' : 'hover:bg-blue-500 hover:text-white'}`}
                 >
