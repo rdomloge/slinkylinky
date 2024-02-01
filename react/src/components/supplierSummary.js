aa89ea213881131dc17709d8d34559ad6e9ccf6f
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-01 11:32:21 +0000

    Refactor supplier name display in SupplierSummary component

--- react/src/components/supplierSummary.js
+++ react/src/components/supplierSummary.js
@@ -3,11 +3,15 @@ export default function SupplierSummary({supplier}) {
 
         return(
             <div>
-                <div>
+                {supplier.thirdParty ? 
+                    <span className="mr-2 text-2xl">{supplier.name}</span> 
+                : 
+                    <>
                     <span className="mr-2 text-2xl">{supplier.domain}</span>
                     <span className="mr-2 text-2xl">DA-{supplier.da}</span>
                     <span className="mr-2 text-2xl">{supplier.weWriteFeeCurrency}{supplier.weWriteFee}</span>
-                </div>
+                    </>
+            ***REMOVED***
                 
             </div>
         );
