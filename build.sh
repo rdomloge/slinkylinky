13c5ba8a6a4d410eaf98a5cb6a595ddee786257b
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-14 20:25:56 +0000

    Version 3.0

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mvn -Dmaven.test.skip=true clean package && \
 docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64\
- -t rdomloge/slinky-linky-audit:2.1 . --push
+ -t rdomloge/slinky-linky-audit:3.0 . --push
