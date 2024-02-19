071c5eef7cdfd33269ae1f2bf3d42c96c0115c29
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-19 14:41:55 +0000

    Remove flowbite-react plugin from tailwind.config.js

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './node_modules/flowbite-react/lib/**/*.js',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,8 +13,5 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  }
 }
