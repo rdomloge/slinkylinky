f92b01533dd7740ebad5389ec67da2664e068ed2
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-27 13:12:25 +0000

    Add event handlers for create and delete operations

--- src/main/java/com/domloge/slinkylinky/linkservice/postprocessing/ProposalEventDispatcher.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/postprocessing/ProposalEventDispatcher.java
@@ -2,6 +2,8 @@ package com.domloge.slinkylinky.linkservice.postprocessing;
 
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.HandleAfterDelete;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
@@ -10,11 +12,7 @@ import com.domloge.slinkylinky.events.ProposalUpdateEvent;
 import com.domloge.slinkylinky.events.ProposalUpdateEvent.ProposalEventType;
 import com.domloge.slinkylinky.linkservice.entity.Proposal;
 import com.domloge.slinkylinky.linkservice.entity.Supplier;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,18 +24,30 @@ public class ProposalEventDispatcher {
     @Autowired
     private AmqpTemplate proposalsRabbitTemplate;
 
-    
  
     @HandleAfterSave
     public void handleAfterSave(Proposal proposal) throws JsonProcessingException {
         log.info("Proposal {} has been saved", proposal.getId());
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.setSerializationInclusion(Include.NON_NULL);
+        
+        doCommon(proposal, new ProposalUpdateEvent(ProposalEventType.UPDATED));
+    }
 
+    @HandleAfterDelete
+    public void handleAfterDelete(Proposal proposal) throws JsonProcessingException {
+        log.info("Proposal {} has been deleted", proposal.getId());
+        
+        doCommon(proposal, new ProposalUpdateEvent(ProposalEventType.DELETED));
+    }
+
+    @HandleAfterCreate
+    public void handleAfterCreate(Proposal proposal) throws JsonProcessingException {
+        log.info("Proposal {} has been created", proposal.getId());
+        
+        doCommon(proposal, new ProposalUpdateEvent(ProposalEventType.CREATED));
+    }
+
+    private void doCommon(Proposal proposal, ProposalUpdateEvent event) {
         Supplier supplier = proposal.getPaidLinks().get(0).getSupplier();
-        ProposalUpdateEvent event = new ProposalUpdateEvent(ProposalEventType.UPDATED);
         event.setProposalDetails(proposal.getArticle(), proposal.getId());
         event.setSupplierDetails(supplier.getName(), supplier.getEmail(), supplier.getWebsite(), supplier.getWeWriteFee(), 
             supplier.getWeWriteFeeCurrency(), supplier.isThirdParty());
