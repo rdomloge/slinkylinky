f8921f3f8f2284718cf520eb333a7d27e5ef892b
Ramsay.Domloge <rdomloge@gmail.com>
2024-02-27 13:04:16 +0000

    Update demand and supplier data

---
+++ testcafe/tests/proposalDetail.js
@@ -0,0 +1,14 @@
+import { createNewDemand, createSupplier } from './helper';
+
+fixture("Proposal Detail Page")
+    .page("http://localhost:3000/demand");
+
+test("Proposal can be created", async t=> {
+
+    // await t.useRole(gitHubUser)
+
+    await createSupplier(1);
+
+    await createNewDemand(5);
+
+})
