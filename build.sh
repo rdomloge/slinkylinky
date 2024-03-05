6d2cff22cda902ac4de8917ff8c497d68dded34e
Ramsay.Domloge <rdomloge@gmail.com>
2024-03-05 08:39:05 +0000

    Update version to 3.6 in build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mvn -Dmaven.test.skip=true clean package && \
 docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64\
- -t rdomloge/slinky-linky-linkservice:3.4 . --push
+ -t rdomloge/slinky-linky-linkservice:3.6 . --push
