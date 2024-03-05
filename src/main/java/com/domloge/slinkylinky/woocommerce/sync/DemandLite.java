e8085a38f4743919f2365df22e182fada546eb4c
Ramsay.Domloge <rdomloge@gmail.com>
2024-03-05 08:37:12 +0000

    Add DemandLite class

---
+++ src/main/java/com/domloge/slinkylinky/woocommerce/sync/DemandLite.java
@@ -0,0 +1,14 @@
+package com.domloge.slinkylinky.woocommerce.sync;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DemandLite {
+    private int daNeeded;
+
+    private String anchorText;
+
+    private String url;
+}
