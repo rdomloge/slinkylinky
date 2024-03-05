3c7fbb4f219413f33559e09707d47e46a6847654
Ramsay.Domloge <rdomloge@gmail.com>
2024-03-05 08:41:01 +0000

    Add validDomain function and url_domain function to Util.js

--- react/src/components/Util.js
+++ react/src/components/Util.js
@@ -4,3 +4,19 @@ export function addProtocol(url) {
     }
     return url;
 }
+
+export function validDomain(url) {
+    const urlRegex = /^(((http|https):\/\/|)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?)$/;
+    if (urlRegex.test(url)) {
+         return true;
+     } else {
+         return false;
+     }
+}
+
+function url_domain(data) {
+    let domain =  new URL(addProtocol(data))
+    let hostname = domain.hostname
+    return hostname.replace('www.','');
+}
+
