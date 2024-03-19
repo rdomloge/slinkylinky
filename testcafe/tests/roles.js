7ffe58f8c60b62cea04606f9313f3ab3580a9e2b
Ramsay.Domloge <rdomloge@gmail.com>
2024-03-19 17:09:42 +0000

    Moved to environment variables

--- testcafe/tests/roles.js
+++ testcafe/tests/roles.js
@@ -6,12 +6,15 @@ export const gitHubUser = Role('http://localhost:3000/api/auth/signin', async t 
     
     await t.click('body > div.page > div > div > div:nth-child(2) > form > button');
     
-    await t.typeText('#login_field', 'rdomloge@gmail.com');
-    await t.typeText('#password', 'Badmuthafucka0');
+    await t.typeText('#login_field', process.env.githubUsername);
+    await t.typeText('#password', process.env.githubPassword);
     
     await t.click('#login > div.auth-form-body.mt-3 > form > div > input.btn.btn-primary.btn-block.js-sign-in-button');
 
-    const reAuthButton = Selector('#js-oauth-authorize-btn');
+    const loginError = Selector('#js-flash-container > div > div > div');
+    await t.expect(loginError.exists).notOk();
+
+    const reAuthButton = Selector('button.js-oauth-authorize');
 
     if(await reAuthButton.exists) {
         await t.wait(1000)
