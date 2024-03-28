9fccb479b24765267db00cb0ee0f5afe658b3319
Ramsay.Domloge <rdomloge@gmail.com>
2024-03-28 15:20:56 +0000

    Update database scripts and REST API endpoints

--- db-scripts/restore-from-backup.sh
+++ db-scripts/restore-from-backup.sh
@@ -6,7 +6,7 @@
 ## THE DATABASES MUST ALREADY EXIST AND BE OWNED BY SLINKYLINKY
 
 docker run -ti --rm --env PGPASSWORD=sl --entrypoint psql -v C:/Users/domloger/Downloads/temp:/backup -ti postgres \
--f backup/2024-03-04_1600-backup.sql -h host.docker.internal -U slinkylinky postgres
+-f backup/2024-03-27_1000-backup.sql -h host.docker.internal -U slinkylinky postgres
 
 docker run -ti --rm --env PGPASSWORD=sl --entrypoint psql -v C:/Users/domloger/repos/chris/linkservice/db-scripts:/backup -ti postgres \
--f backup/v3.6-sql-changes.sql -h host.docker.internal -U slinkylinky woo
+-f backup/v3.9-sql-changes.sql -h host.docker.internal -U slinkylinky slinkylinky
