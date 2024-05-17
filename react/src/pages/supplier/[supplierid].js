7b98e6b4cf2be6879775ddcf9f9fe322254acc6f
Ramsay.Domloge <rdomloge@gmail.com>
2024-05-17 14:04:28 +0100

    Forgotten/missed commit form the bulk supplier upload

--- react/src/pages/supplier/[supplierid].js
+++ react/src/pages/supplier/[supplierid].js
@@ -7,6 +7,11 @@ import { useRouter } from 'next/router'
 export default function EditSupplier() {
     const router = useRouter()
     const [supplier, setSupplier] = useState()
+    const [supplierName, setSupplierName] = useState('');
+    const [supplierWebsite, setSupplierWebsite] = useState('')
+    const [supplierSource, setSupplierSource] = useState('')
+    const [supplierEmail, setSupplierEmail] = useState('')
+    const [supplierWeWriteFee, setSupplierWeWriteFee] = useState(0)
 
     useEffect(
         () => {
@@ -19,15 +24,25 @@ export default function EditSupplier() {
                 .then( (s) => {
                     s.id = router.query.supplierid
                     setSupplier(s);
+                    setSupplierName(s.name);
+                    setSupplierWebsite(s.website);
+                    setSupplierSource(s.source);
+                    setSupplierEmail(s.email);
+                    setSupplierWeWriteFee(s.weWriteFee);
                 });
             }
         }, [router.isReady, router.query.supplierid]);
 
     return (
         <Layout>
-            <PageTitle title="Edit supplier"/>
+            <PageTitle id="supplier-edit-id" title="Edit supplier"/>
             {supplier ?
-                <AddOrEditSupplier supplier={supplier}/>
+                <AddOrEditSupplier supplier={supplier} 
+                    supplierName={supplierName} setSupplierName={setSupplierName} 
+                    supplierWebsite={supplierWebsite} setSupplierWebsite={setSupplierWebsite}
+                    supplierFee={supplierWeWriteFee} setSupplierFee={setSupplierWeWriteFee}
+                    supplierSource={supplierSource} setSupplierSource={setSupplierSource}
+                    supplierEmail={supplierEmail} setSupplierEmail={setSupplierEmail}/>
             : null}
         </Layout>
     )
