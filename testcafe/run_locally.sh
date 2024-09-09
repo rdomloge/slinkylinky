c5230f25850272fe949d4537899798c842da5bc9
Ramsay.Domloge <rdomloge@gmail.com>
2024-09-09 20:36:43 +0100

    refactor: Update testcafe run script to include GitHub credentials and use host.docker.internal
    This commit updates the testcafe run script to include GitHub credentials for authentication and uses the host.docker.internal as the host. This change allows for smoother integration with GitHub and ensures proper connectivity when running the tests locally.

--- testcafe/run_locally.sh
+++ testcafe/run_locally.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 
-docker run -ti --rm --mount type=bind,source="/c/Users/domloger/repos/chris/linkservice/testcafe/tests",target=/tests testcafe/testcafe chromium /tests/**/*.js --live
+docker run -ti --rm -e githubUsername=[USERNAME] \
+-e githubPassword=[PASSWORD] \
+-e HOST=host.docker.internal \
+--mount type=bind,source="/c/Users/domloger/repos/chris/adminwebsite/testcafe/tests",target=/tests testcafe/testcafe chromium /tests/**/*.js --live
+ 
