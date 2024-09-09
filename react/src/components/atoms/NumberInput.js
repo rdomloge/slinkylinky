20109f05e844c870539d40d76a97fecfb9a99ffd
Ramsay.Domloge <rdomloge@gmail.com>
2024-09-09 20:30:17 +0100

    refactor: Update NumberInput component to include min and max attributes
    The NumberInput component in the atoms folder has been updated to include the min and max attributes. This allows for setting the minimum and maximum values that can be entered in the input field. The default values for min and max are 0 and 1000000 respectively. This change ensures better control over the input range for number values.

--- react/src/components/atoms/NumberInput.js
+++ react/src/components/atoms/NumberInput.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 import LockedImage from '@/components/locked.svg';
 import { useState } from "react";
 
-export default function NumberInput({label, disabled, binding, changeHandler, id, step = 1}) {
+export default function NumberInput({label, disabled, binding, changeHandler, id, step = 1, min = 0, max = 1000000}) {
 
     return (
-        <div className="w-full px-2 mb-6 md:mb-0 border-b border-teal-500">
+        <div className="w-full px-2 mb-6 md:mb-0 border-b border-teal-500 mt-4">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-name">
                 {label}
                 {disabled ?
@@ -20,7 +20,9 @@ export default function NumberInput({label, disabled, binding, changeHandler, id
                 value={binding}
                 step={step}
                 className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-1 leading-tight focus:outline-none"
-                id={id}/>
+                id={id}
+                min={min}
+                max={max}/>
         </div>
     );
 }
