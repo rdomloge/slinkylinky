d9384dcc2bdb8d56d0b4014273d5f898048446bc
Ramsay.Domloge <rdomloge@gmail.com>
2024-10-22 13:09:21 +0100

    Refactor: Update slinky-linky-adminwebsite version to 5.7 in build-adminwebsite.sh

--- build-adminwebsite.sh
+++ build-adminwebsite.sh
@@ -4,4 +4,4 @@
 # -t rdomloge/slinky-linky-adminwebsite:3.1 .
 
 docker buildx build --push --platform linux/amd64,linux/arm64\
- -t rdomloge/slinky-linky-adminwebsite:5.6 .
+ -t rdomloge/slinky-linky-adminwebsite:5.7 .
