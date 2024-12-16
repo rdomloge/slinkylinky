a32c789a913f96c580540500a03a4c9db677fe0f
Ramsay.Domloge <rdomloge@gmail.com>
2024-12-16 10:24:14 +0000

    Enable scheduling in SupplierengagementApplication

--- src/main/java/com/domloge/slinkylinky/supplierengagement/SupplierengagementApplication.java
+++ src/main/java/com/domloge/slinkylinky/supplierengagement/SupplierengagementApplication.java
@@ -3,9 +3,11 @@ package com.domloge.slinkylinky.supplierengagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.domloge.slinkylinky"})
 @EnableAutoConfiguration(exclude={org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class})
+@EnableScheduling
 public class SupplierengagementApplication {
 
 	public static void main(String[] args) {
