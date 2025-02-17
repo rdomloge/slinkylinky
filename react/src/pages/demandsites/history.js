500d749667f406e05e38f3ce6f1d93c0e81797cd
Ramsay.Domloge <rdomloge@gmail.com>
2025-02-17 11:49:43 +0000

    Refactor: Remove unnecessary React import from history.js

--- react/src/pages/demandsites/history.js
+++ react/src/pages/demandsites/history.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import PageTitle from '@/components/pagetitle';
 import Paging from '@/components/Paging';
