95f5ff14ebf663467b41688f0dd5ca37b2f985ac
Ramsay.Domloge <rdomloge@gmail.com>
2025-08-28 15:31:32 +0100

    Fix: Update layout styles in DemandSite component for improved responsiveness

--- react/src/components/DemandSite.js
+++ react/src/components/DemandSite.js
@@ -10,18 +10,18 @@ import { WarningMessage } from "./atoms/Messages";
 export default function DemandSiteSearchResult({demandSite, selectedHandler, id}) {
     return (
         <div className="card list-card flex" id={id}>
-            <div className="flex-0 pr-4 m-auto">
+            <div className="flex-initial pr-4 m-auto w-max">
                 <button onClick={(e) => 
-                        selectedHandler(demandSite)}>
+                        selectedHandler(demandSite)} id="select-demand-site-button">
                 <Image src={ArrowIcon} alt="Arrow icon" height={30} width={30} />
                 </button>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 w-min">
                 <span className="float-right bg-red-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
                     {demandSite.demands.length}
                 </span>
                 <p className="text-lg">{demandSite.name}</p>
-                <p className="mb-6">{demandSite.domain}</p>
+                <p className="mb-6 truncate">{demandSite.domain}</p>
                 {demandSite.categories.filter(c => c.disabled == false).map( (c,index) => <CategoryLite category={c} key={index}/> )}
             </div>
         </div>
