d1842f91a59b4bae464053acd5c479609dc1a6e6
Ramsay.Domloge <rdomloge@gmail.com>
2025-09-12 14:34:42 +0100

    Fix: Remove Google and Github providers from NextAuth configuration

--- react/src/pages/api/auth/[...nextauth].js
+++ react/src/pages/api/auth/[...nextauth].js
@@ -7,14 +7,6 @@ import KeycloakProvider from "next-auth/providers/keycloak";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-    }),
     KeycloakProvider({
         clientId: process.env.KEYCLOAK_ID,
         clientSecret: process.env.KEYCLOAK_SECRET,
