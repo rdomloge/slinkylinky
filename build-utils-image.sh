67462c64e9bdefb92dc1033770f769d33d9c0d8c
Ramsay Domloge <rdomloge@gmail.com>
2025-09-26 14:56:37 +0100

    Add Dockerfile and build script for utils image; update backup.yml to use new image for compression and SCP

--- build-utils-image.sh
+++ build-utils-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64\
- -t rdomloge/scp-sshpass:1.0 -f Dockerfile-scp-sshpass . --push
+ -t rdomloge/utils:1.0 -f Dockerfile-utils . --push
