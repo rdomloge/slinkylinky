7cb9b19b30d1fd712c93b6bde6c77c4350255050
Ramsay Domloge <rdomloge@gmail.com>
2025-10-03 13:57:38 +0100

    Add proposal support endpoint to security whitelist

--- src/main/java/com/domloge/slinkylinky/linkservice/config/SecurityConfig.java
+++ src/main/java/com/domloge/slinkylinky/linkservice/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    private static final String[] AUTH_WHITELIST = {"/actuator/**"};
+    private static final String[] AUTH_WHITELIST = {"/actuator/**", "/.rest/proposalsupport/getArticleFormatted"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,6 +20,8 @@ public class SecurityConfig {
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(Customizer.withDefaults())
             );
+
+        http.headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable()); //disable x-frame-options for all endpoints so they can be iframed in browser iframes
         return http.build();
     }
 }
