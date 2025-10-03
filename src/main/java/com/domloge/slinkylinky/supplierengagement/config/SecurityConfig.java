e913d4972e3f786452214afda6e4f781766661e9
Ramsay Domloge <rdomloge@gmail.com>
2025-10-03 10:30:44 +0100

    Add engagement endpoints to security whitelist in SecurityConfig

--- src/main/java/com/domloge/slinkylinky/supplierengagement/config/SecurityConfig.java
+++ src/main/java/com/domloge/slinkylinky/supplierengagement/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    private static final String[] AUTH_WHITELIST = {"/actuator/**"};
+    private static final String[] AUTH_WHITELIST = {"/actuator/**", "/.rest/engagements/accept", "/.rest/engagements/decline"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
