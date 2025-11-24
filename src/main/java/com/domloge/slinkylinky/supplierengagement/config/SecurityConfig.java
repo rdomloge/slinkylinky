eb698c9ef0b7aa2a87931585f38deb2f321342ad
Ramsay Domloge <rdomloge@gmail.com>
2025-11-24 21:23:10 +0000

    Refactor SecurityConfig to include CSRF configuration for whitelisted endpoints

--- src/main/java/com/domloge/slinkylinky/supplierengagement/config/SecurityConfig.java
+++ src/main/java/com/domloge/slinkylinky/supplierengagement/config/SecurityConfig.java
@@ -13,13 +13,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(AUTH_WHITELIST).permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults())
-            );
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                                .jwt(Customizer.withDefaults())
+                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AUTH_WHITELIST));
         return http.build();
     }
 }
