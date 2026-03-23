7fe1ef2a0ce7c5f6cea7e994c3988a2584cdac53
Ramsay Domloge <ramsay@domloge.com>
2026-03-23 18:10:10 +0000

    Integrate imported services into monorepo build, CI, and configs

--- supplierengagement/run-locally.sh
+++ supplierengagement/run-locally.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# Export required environment variables before running this script.
 docker run \
---env spring_datasource_url='jdbc:postgresql://10.0.0.229:5432/supplierengagement?user=slinkylinky&password=sl' \
--e mail_password=BNZi+X464fPiz4EQcFyg7utUBK6qu+bgyHwSpCeFCr6J \
--e mail_username=AKIA6BJW6V6R3FCUU4UH \
--e mail_host=email-smtp.eu-west-2.amazonaws.com \
--e mail_from=no-reply@slinkylinky.uk \
--e linkservice_baseurl=http://localhost:8080/.rest \
--e slinkylinky_domain=http://localhost:3000 \
--e server_port=8081 \
--e slinkylinky_vhost=dev-slinkylinky \
+-e spring_datasource_url="${spring_datasource_url}" \
+-e mail_password="${mail_password}" \
+-e mail_username="${mail_username}" \
+-e mail_host="${mail_host}" \
+-e mail_from="${mail_from}" \
+-e linkservice_baseurl="${linkservice_baseurl}" \
+-e slinkylinky_domain="${slinkylinky_domain}" \
+-e server_port="${server_port:-8081}" \
+-e slinkylinky_vhost="${slinkylinky_vhost}" \
+-e slinkylinky_rabbitmq_host="${slinkylinky_rabbitmq_host}" \
+-e slinkylinky_rabbitmq_username="${slinkylinky_rabbitmq_username}" \
+-e slinkylinky_rabbitmq_password="${slinkylinky_rabbitmq_password}" \
 --rm rdomloge/slinky-linky-supplierengagement:3.0.1 sl-supplier-engagement 
