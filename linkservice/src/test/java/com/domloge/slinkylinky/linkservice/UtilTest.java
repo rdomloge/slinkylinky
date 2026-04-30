60b06a781e322a9973f703d764a26faf7811279d
Ramsay Domloge <ramsay@domloge.com>
2026-04-30 17:50:44 +0100

    test: add date parsing tests to validate various date formats and error handling

--- linkservice/src/test/java/com/domloge/slinkylinky/linkservice/UtilTest.java
+++ linkservice/src/test/java/com/domloge/slinkylinky/linkservice/UtilTest.java
@@ -1,6 +1,9 @@
 package com.domloge.slinkylinky.linkservice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 
@@ -42,5 +45,35 @@ public class UtilTest {
         // localhost has no public suffix; topPrivateDomain() throws IllegalStateException
         // which is caught and the bare host is returned unchanged
         assertEquals("localhost", Util.stripDomain("http://localhost:8080/app"));
+    }
+
+    @Test
+    void parse_validDate_returnsStartOfDay() {
+        assertEquals(LocalDateTime.of(2024, 3, 15, 0, 0), Util.parse("15/03/2024"));
+    }
+
+    @Test
+    void parse_startOfMonth_returnsStartOfDay() {
+        assertEquals(LocalDateTime.of(2024, 1, 1, 0, 0), Util.parse("01/01/2024"));
+    }
+
+    @Test
+    void parse_endOfMonth_returnsStartOfDay() {
+        assertEquals(LocalDateTime.of(2024, 1, 31, 0, 0), Util.parse("31/01/2024"));
+    }
+
+    @Test
+    void parse_leapYearDate_returnsStartOfDay() {
+        assertEquals(LocalDateTime.of(2024, 2, 29, 0, 0), Util.parse("29/02/2024"));
+    }
+
+    @Test
+    void parse_invalidFormat_throwsDateTimeParseException() {
+        assertThrows(Exception.class, () -> Util.parse("15-03-2024"));
+    }
+
+    @Test
+    void parse_invalidDay_throwsDateTimeParseException() {
+        assertThrows(Exception.class, () -> Util.parse("32/01/2024"));
     }
 }
